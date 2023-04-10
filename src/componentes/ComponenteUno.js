815735917ca00d8808031257361a7dc21d2725df
GiselaFlores <gi.flores@bue.edu.ar>
2023-04-10 10:31:17 -0300

    routes

---
+++ src/componentes/ComponenteUno.js
@@ -0,0 +1,13 @@
+import {Link as LinkRouter} from 'react-router-dom';
+
+
+const ComponenteUno = () => {
+
+    return(
+    
+        <LinkRouter to={'./AboutMe'} />
+        
+    )
+}
+
+export default ComponenteUno;
